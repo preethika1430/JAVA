391c9d9476e08a5157f124935c4c77c79a26e5cc
preethika1430 <preethikajena@gmail.com>
2026-02-02 09:45:53 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,14 @@
+class Person{
+    String name;
+
+    void display(){
+        System.out.println("Name:" + name);
+    }
+    public static void main(String[] args){
+        Person p = new Person();
+        p.name="John";
+        p.display();
+    }
+}
+    
+
