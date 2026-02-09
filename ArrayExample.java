e2cc515dd481851d338ff57e699d1770dd870c53
preethika1430 <preethikajena@gmail.com>
2026-02-09 09:22:18 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,14 @@
+public class ArrayExample{
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
+
