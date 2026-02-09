82d97d795c8906a84299795d98289ffccab8ae60
preethika1430 <preethikajena@gmail.com>
2026-02-09 09:11:58 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,10 @@
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+
+        System.out.println("Contains Java? " + str.contains("Java"));
+        System.out.println("Replaced java with python: " + str.replace("Java","Python"));
+    }
+}
+
